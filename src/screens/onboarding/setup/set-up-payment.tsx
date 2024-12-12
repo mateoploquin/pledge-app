acda5292a563a37d966b859fe478f62565bfb75f
momo <mozeart@hotmail.com>
2024-12-12 18:44:24 +0100

    Added code to check if apple pay is supported

--- src/screens/onboarding/setup/set-up-payment.tsx
+++ src/screens/onboarding/setup/set-up-payment.tsx
@@ -2,9 +2,8 @@ import React, { useEffect, useState } from "react";
 import { View, Text, Alert } from "react-native";
 import colors from "../../../theme/colors";
 import MainButton from "../../../components/buttons/main-button";
-import { useStripe } from "@stripe/stripe-react-native";
+import { useStripe, isPlatformPaySupported } from "@stripe/stripe-react-native";
 import { fetchPaymentSheetParams } from "../../../services/stripe-api";
-import { onAuthStateChanged, getIdToken } from "firebase/auth";
 import { auth } from '../../../../firebaseConfig';
 import { sendPledgeData } from "../../../services/sendPledgeData";
 
@@ -29,6 +28,15 @@ const SetPayment: React.FC<SetPaymentProps> = ({
 }) => {
   const { initPaymentSheet, presentPaymentSheet } = useStripe();
   const [loading, setLoading] = useState(false);
+  const [isApplePaySupported, setIsApplePaySupported] = useState(false);
+
+  useEffect(() => {
+    const checkApplePaySupport = async () => {
+      const isSupported = await isPlatformPaySupported();
+      setIsApplePaySupported(isSupported);
+    };
+    checkApplePaySupport();
+  }, []);
 
   const initializePaymentSheet = async (): Promise<boolean> => {
     try {
@@ -131,12 +139,14 @@ const SetPayment: React.FC<SetPaymentProps> = ({
       </Text>
 
       <View style={{ marginVertical: 20, paddingVertical: 10 }}>
-        <MainButton
-          onPress={handleSetUpPayment}
-          text={paymentSetupComplete ? "Update Payment Method" : "Set Up Payment"}
-          style={{ opacity: loading ? 0.5 : 1 }}
-          disabled={loading}
-        />
+        {isApplePaySupported && (
+          <MainButton
+            onPress={handleSetUpPayment}
+            text={paymentSetupComplete ? "Update Payment Method" : "Set Up Payment"}
+            style={{ opacity: loading ? 0.5 : 1 }}
+            disabled={loading}
+          />
+        )}
         {paymentSetupComplete && (
           <Text
             style={{
